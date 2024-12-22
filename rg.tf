af0cb40f434f4de89e77bf9afceefa6de34a821a
sudhakar7 <sudhakarreddy0085@gmail.com>
2024-12-22 21:52:30 +0530

    added

--- rg.tf
+++ rg.tf
@@ -5,3 +5,10 @@ resource "azurerm_resource_gruop_name" "example"{
         name = sudhakar
     }
 }
+resource "azurerm_resource_gruop_name" "example"{
+    name = "n1"
+    location = "central india"
+    tags{
+        name = sudhakar
+    }
+}
